95aff89ae6f2286f7cb648bb77e8121f5fcf29f7
Ermol <00ermzahar@gmail.com>
2025-04-07 17:20:04 +0300

    Add unit test for write_to_file function

--- tests/test_io/test_output.py
+++ tests/test_io/test_output.py
@@ -1,7 +1,9 @@
+import os
 import unittest
 import io
 from contextlib import redirect_stdout
-from app.io.output import print_to_console
+from app.io.output import print_to_console, write_to_file
+
 
 class TestOutputFunctions(unittest.TestCase):
     def test_print_to_console(self):
@@ -9,6 +11,18 @@ class TestOutputFunctions(unittest.TestCase):
         with redirect_stdout(captured_output):
             print_to_console("Test message")
         self.assertEqual(captured_output.getvalue().strip(), "Test message")
+
+    def test_write_to_file(self):
+        test_path = "test_output.txt"
+        test_text = "File output test"
+
+        write_to_file(test_path, test_text)
+
+        with open(test_path, "r", encoding="utf-8") as f:
+            content = f.read()
+
+        os.remove(test_path)
+        self.assertEqual(content, test_text)
 
 if __name__ == "__main__":
     unittest.main()
